552f7c6c3971ff2ca97680ee7827998e2d63a88b
Pandia008 <rajan.pandia24@gmail.com>
2020-09-08 22:54:39 +0530

    NewSample2

---
+++ NewSample2.java
@@ -0,0 +1,8 @@
+class NewSample2{
+	public static void main (String []args){
+		String name = "asNewSample2JavaCLass";
+		String type = "svdNewSample2GitTesting";
+		String type1 = "svdNewSample2GitTesting";
+		System.out.println(name+" :  "+type+type1);
+	}
+}
